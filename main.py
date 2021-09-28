f7cb3298af790d469ee90f8a035ad21f06e14889
joetjo <joetjo@users.noreply.github.com>
2021-09-28 13:47:09 +0200

    Bug fix : allows to reinit setup

--- main.py
+++ main.py
@@ -12,6 +12,5 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from markdown.markdown import markdown
-
-markdown()
+from markdown.markdown import MarkdownHelper
+MarkdownHelper().markdown()
